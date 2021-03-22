1af8f7c1f6e8eb9f603cfb2b689c61e90645313c
vadgun <vadgun@hotmail.com>
2021-03-22 10:43:57 -0600

    db

--- application/config/database.php
+++ application/config/database.php
@@ -78,9 +78,9 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'sivem',
+	'username' => 'notil830_sivem-user',
+	'password' => 'sivem-user',
+	'database' => 'notil830_sivem',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
@@ -96,5 +96,6 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+
 
 
