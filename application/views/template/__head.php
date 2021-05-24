cb31ddcd3f9c95f9eb52f2077570941aa0626dbe
vadgun <vadgun@hotmail.com>
2021-05-24 08:29:23 -0500

    jquery

--- application/views/template/__head.php
+++ application/views/template/__head.php
@@ -10,6 +10,7 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/alertifyjs/css/alertify.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/alertifyjs/css/themes/default.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/nabar.css">
+	<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 	<script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
 
 	<script src="<?php echo base_url()?>assets/bootstrap/js/bootstrap.js"></script>
