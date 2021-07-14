e77c0bd66657dd025a43f1f64eb4bd80b755f7c9
vadgun <vadgun@hotmail.com>
2021-07-14 10:02:23 -0500

    librerias modificadas

--- application/views/login.php
+++ application/views/login.php
@@ -56,7 +56,6 @@
 </div>
 
 </body>
-<script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
 <script src="<?php echo base_url('assets/js/login.js')?>"></script>
 
 </html>
